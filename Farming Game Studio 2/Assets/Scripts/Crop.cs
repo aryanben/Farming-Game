0c2a6cc4aa647f1ea501fcf85d5ce5ba2a4c56b2
aryanben <a.bendrey@saedubai.com>
2020-03-31 03:58:28 +0530

    Farm assets

---
+++ Farming Game Studio 2/Assets/Scripts/Crop.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crop : MonoBehaviour
+{
+    public int currentDay;
+    public int cropPlantDate;
+    public int cropAge;
+    public string CropName;
+    bool watered;
+    public int halfAge;
+
+    public GameObject cropFruit;
+    public GameObject cropPlantSmall;
+    public GameObject cropPlantBig;
+    public enum State
+    {
+        Planted,
+
+        Ready
+
+    }
+    public State cropState;
+    private void Update()
+    {
+        DayUpdate();
+    }
+    public void DayUpdate()
+    {
+        cropAge = currentDay - cropPlantDate;
+        if (!watered)
+        {
+            cropAge--;
+        }
+        if (cropAge >= halfAge)
+        {
+            cropPlantSmall.SetActive(false);
+            cropPlantBig.SetActive(true);
+
+        }
+        if (cropAge >= (halfAge * 2))
+        {
+            cropState = State.Ready;
+            cropFruit.SetActive(true);
+        }
+    }
+    public void Water() 
+    {
+        watered = true;
+        //make soil darker
+    }
+    public void Harvest() 
+    {
+        if (cropState==State.Ready)
+        {
+
+        }
+    }
+
+
+}
