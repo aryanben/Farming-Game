3a255036b97d682315fa5f8f550929c754c2766a
aryanben <a.bendrey@saedubai.com>
2020-04-27 22:39:04 +0530

    Inventory and some other things

---
+++ Farming Game Studio 2/Assets/Scripts/Ben/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum typeEnum
+    {
+        Wood,
+        Stone,
+        Seed
+    }
+    public typeEnum type;
+    public int amount;
+}
